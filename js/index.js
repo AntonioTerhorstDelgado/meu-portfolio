409013971c9ddb50e7fc07ef4768fdc2eaf188d4
Antônio Terhorst <84198804+AntonioTerhorstDelgado@users.noreply.github.com>
2025-01-04 15:51:15 +0000

    adicionando um detalhe na minha `foto`

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var profilePicture = document.getElementById("profilePicture");
+
+  profilePicture.addEventListener("mouseover", function () {
+    profilePicture.style.transform = "scale(1.5) translateX(-50%)";
+  });
+
+  profilePicture.addEventListener("mouseout", function () {
+    profilePicture.style.transform = "scale(1) translateX(-50%)";
+  });
+});
