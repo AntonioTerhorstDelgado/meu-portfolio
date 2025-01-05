54f9759214fa0065691e4b517176a5015393d60e
Antônio Terhorst <84198804+AntonioTerhorstDelgado@users.noreply.github.com>
2025-01-05 21:29:13 +0000

    Fixing a `index.js` effect

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var profilePicture = document.getElementById("profilePicture");
 
   profilePicture.addEventListener("mouseover", function () {
-    profilePicture.style.transform = "scale(1.5) translateX(-50%)";
+    profilePicture.style.transform = "scale(1.5) translateX(-35%)";
   });
 
   profilePicture.addEventListener("mouseout", function () {
